b79a086d5124e52a429bd7f9de1c77e7d3b34dbf
Abraão Santana <deploys.app@gmail.com>
2021-08-06 22:48:17 -0300

    Criação Projeto - Contract First
    - Projeto inicial com build e geração de classes com jaxb

---
+++ src/main/java/br/com/devweb/soapws/SpringbootSoapWsApplication.java
@@ -0,0 +1,13 @@
+package br.com.devweb.soapws;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootSoapWsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootSoapWsApplication.class, args);
+	}
+
+}
